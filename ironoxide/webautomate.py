8bf96d025cb65b0cb5e7eb21bc87e5c70f7a7e8a
Sulaiman Ghori <skghori03@gmail.com>
2022-04-04 09:00:38 +0200

    Wrote much more structured classes and debugged

--- ironoxide/webautomate.py
+++ ironoxide/webautomate.py
@@ -21,16 +21,93 @@ logging.basicConfig(level=settings.LOGGING_LEVEL, format=('%(asctime)s %(levelna
 
 creds = json.load(open(HERE.parent/'data'/'creds.json'))
 
+
 class IU_PageElement():
     def __init__(self, title: str, element: BeautifulSoup):
         self.title = title
         self.element = element
 
-class IU_Test(IU_PageElement):
-    def __init__(self, title: str, element: BeautifulSoup, completable: bool = False, completed: bool = False):
+class Course(IU_PageElement):
+    def __init__(self, title: str, element: BeautifulSoup):
         super().__init__(title, element)
-        self.completable = completable
-        self.completed = completed
+        self.url = element['href']
+
+    # -- Course Subclasses --
+    class Test(IU_PageElement):
+        def __init__(self, course, title: str, element: BeautifulSoup, completable: bool = False, completed: bool = False):
+            super().__init__(title, element)
+            self.course: Course = course
+            self.completable = completable
+            self.completed = completed
+
+        class Question(IU_PageElement):
+            def __init__(self, test, title: str, element: BeautifulSoup):
+                super().__init__(title, element)
+                self.test: Course.Test = test
+                self.answered = False
+
+                class Answer(IU_PageElement):
+                    def __init__(self, question, title: str, element: BeautifulSoup):
+                        super().__init__(title, element)
+                        self.question: Course.Test.Question = question
+                        self.correct = False
+
+        def do_test(self, driver: uc.Chrome):
+            # wait for load
+            test_main_page = BeautifulSoup(WebDriverWait(driver, TIMEOUT).until(ec.presence_of_element_located((By.XPATH, "//div[contains(@role, 'main')]"))).get_attribute('innerHTML'), features='lxml')
+
+            e = test_main_page.find('div', {'class': 'quizinfo'})
+            # ensure we are in a Highest Grade test and proceed
+            if  'Grading method: Highest grade' not in test_main_page.find('div', {'class': 'quizinfo'}).text:
+                logger.error(f'Not in a Highest Grade test ({self.title}), unsafe to continue, exiting..')
+                driver.quit()
+            
+            # proceed into the test
+            driver.find_element(By.XPATH, "//button[contains(@type, 'submit')]").click()
+            # NOTE for next time to pick up on, so we now need to populate the questions and answers, and create a method to select them. after that we need to store the selected answers in a csv file. or somethign. data storage is two sessions down the line.
+
+
+    # -- Course Methods --
+    def populate_tests(self, driver: uc.Chrome):
+        """ Populates the tests for this course by searching the activity pane. Assumes driver is on the course page """
+        
+        # Find ONLINE TESTS AND EVALUATION
+        activitiesPane = BeautifulSoup(WebDriverWait(driver, TIMEOUT).until(ec.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'ctopics topics bsnewgrid row')]"))).get_attribute('innerHTML'), features='lxml')
+        activities = [IU_PageElement(str(x.find('h3', {'class': 'sectionname'}).text).strip(), x) for x in list(activitiesPane.find_all('li', {'class': ['section', 'main']}))]
+
+        for activity in activities:
+            if 'online tests and evaluation' in str(activity.element.text).lower():
+                test_activity = activity
+                break
+        else:
+            selection = getValidSelection([x.title for x in activities], 'Online Tests and Evaluation not found, please select manually or exit')
+            test_activity = activities[selection]
+
+        # Expand test panel if not already expanded
+        the_toggle = test_activity.element.find('span', {'class': 'the_toggle'})
+        if the_toggle['aria-expanded'] == 'false':
+            logger.info('Expanding tests panel..')
+            element = driver.find_element(By.ID, the_toggle['id'])
+            element.click()
+
+        # Get list of tests and test titles from test panel
+        self.tests = [Course.Test(self, str(x.find('span', {'class': 'instancename'}).text).strip(), x) for x in list(test_activity.element.find('ul', {'class': ['section', 'img-text']}).find_all('li'))]
+        
+        # Check if any tests are completed. If so, mark them as such
+        for test in self.tests:
+            # ensure we can complete this test
+            if test.element.find('div', {'class': 'availabilityinfo isrestricted'}) is None and test.element.find('span', {'class': 'autocompletion'}) is not None:
+                logger.info(f'{test.title} is completable..')
+                test.completable = True
+                # then check if it is completed
+                completion = next(test.element.find('span', {'class': 'autocompletion'}).children).attrs['alt']
+                if next(test.element.find('span', {'class': 'autocompletion'}).children).attrs['alt'][:10] == 'Completed:':
+                    logger.info(f'{test.title} is completed!')
+                    test.completed = True
+                else:
+                    logger.info(f'{test.title} is not completed..')
+            else:
+                logger.info(f'{test.title} is not completable..')
 
 def getValidSelection(selectables: list[str], query_string: str, default_selection_i: int = 0) -> int:
     """_summary_
@@ -76,24 +153,25 @@ def main():
     # -- Login --
 
     driver.get(settings.DASH_URL)
-    # we must login, i didnt get cookies working yet
+    # We must login, i didnt get cookies working yet
     if driver.current_url != settings.DASH_URL:
-        # wait for page load
+        # Wait for page load
+        logger.info('Logging in..')
         try:
             element = WebDriverWait(driver, 3).until(ec.presence_of_element_located((By.ID, 'username')))
         except TimeoutException:
             logger.error("Loading took too much time!")
-        # fill in user & pass fields
+        # Fill in user & pass fields
         for fieldName in ['username', 'password']:  # fill in user and password
             field = driver.find_element(By.ID, fieldName)
             field.clear()
             field.send_keys(creds[fieldName])
         # hit login
         driver.find_element(By.ID, 'loginbtn').click()
-        logger.info('Logged in..')
+        logger.info('Logged in!')
 
     # -- Dashboard --
-    # ensure dashboard load - this commonly fails
+    # Ensure dashboard load - this commonly fails
     for attempt in range(5):
         try:
             # deny cookies
@@ -106,59 +184,24 @@ def main():
         else:
             break
 
-    # get list of active courses
+    # Get list of active courses
     coursesPane = BeautifulSoup(WebDriverWait(driver, TIMEOUT).until(ec.presence_of_element_located((By.ID, 'courses-active'))).get_attribute('innerHTML'), features='lxml')
-    courses = [{'title': str(x.text).strip().split('\n')[1], 'element': x} for x in list(coursesPane.find_all('a', {'class': 'courseitem'}))]
+    courses = [Course(str(x.text).strip().split('\n')[1], x) for x in list(coursesPane.find_all('a', {'class': 'courseitem'}))]
 
-    # ensure we get a valid selection
-    selection = getValidSelection(([x['title'] for x in courses]), 'Select course', 0)
+    # Ensure we get a valid selection
+    selection = getValidSelection(([x.title for x in courses]), 'Select course', 0)
     course = courses[selection]
 
     # -- Course --
-    # go to course page
+    # Go to course page
     logger.info('Going to course page..')
-    driver.get(str(course['element']['href']))
-
-    # find ONLINE TESTS AND EVALUATION
-    activitiesPane = BeautifulSoup(WebDriverWait(driver, TIMEOUT).until(ec.presence_of_element_located((By.XPATH, "//ul[contains(@class, 'ctopics topics bsnewgrid row')]"))).get_attribute('innerHTML'), features='lxml')
-    activities = [IU_PageElement(str(x.find('h3', {'class': 'sectionname'}).text).strip(), x) for x in list(activitiesPane.find_all('li', {'class': ['section', 'main']}))]
-
-    for activity in activities:
-        if 'online tests and evaluation' in str(activity.element.text).lower():
-            test_activity = activity
-            break
-    else:
-        selection = getValidSelection([x.title for x in activities], 'Online Tests and Evaluation not found, please select manually or exit')
-        test_activity = activities[selection]
-
-    # expand test panel if not already expanded
-    the_toggle = test_activity.element.find('span', {'class': 'the_toggle'})
-    if the_toggle['aria-expanded'] == 'false':
-        logger.info('Expanding tests panel..')
-        element = driver.find_element(By.ID, the_toggle['id'])
-        element.click()
-
-    # get list of tests and test titles from test panel
-    tests = [IU_Test(str(x.find('span', {'class': 'instancename'}).text).strip(), x) for x in list(test_activity.element.find('ul', {'class': ['section', 'img-text']}).find_all('li'))]
+    driver.get(str(course.url))
     
-    # check if any tests are completed. this could be done inline but that becomes hard to read.
-    for test in tests:
-        # ensure we can complete this test
-        if test.element.find('div', {'class': 'availabilityinfo isrestricted'}) is None and test.element.find('span', {'class': 'autocompletion'}) is not None:
-            logger.info(f'{test.title} is completable..')
-            test.completable = True
-            # then check if it is completed
-            if test.element.find('span', {'class': 'autocompletion'}).text[:10] == 'Completed:':
-                logger.info(f'{test.title} is completed!')
-                test.completed = True
-            else:
-                logger.info(f'{test.title} is not completed..')
-        else:
-            logger.info(f'{test.title} is not completable..')
-
+    # get tests
+    course.populate_tests(driver)
 
     # go to the first incomplete test
-    for test in tests:
+    for test in course.tests:
         if test.completable and not test.completed:
             logger.info(f'Going to test {test.title}..')
             driver.get(str(test.element.find('a')['href']))
@@ -166,14 +209,8 @@ def main():
 
     # -- Test --
 
-    # ensure we are in a Highest Grade test and proceed
-    if 'Grading method: Highest grade' not in test.element.find('div', {'role': 'main'}).text:
-        logger.error(f'Not in a Highest Grade test ({test.title}), unsafe to continue, exiting..')
-        driver.quit()
+    test.do_test(driver)
     
-    # proceed into the test
-    driver.find_element(By.XPATH, "//button[contains(@type, 'submit')]").click()
-
     logger.info('Done!')
     driver.quit()
 
